0c00608196e4cb06e5908131911a6f8a446e4843
EkanshBatra <>
2024-07-29 10:59:19 +0530

    Socket exit message update

--- Assets/Scripts/APIs/SocketIOManager.cs
+++ Assets/Scripts/APIs/SocketIOManager.cs
@@ -226,9 +226,31 @@ public class SocketIOManager : MonoBehaviour
 
     internal void CloseSocket()
     {
+        CloseSocketMesssage("EXIT");
         if (this.manager != null)
         {
             this.manager.Close();
+        }
+    }
+    private void CloseSocketMesssage(string eventName)
+    {
+        // Construct message data
+
+        ExitData message = new ExitData();
+        message.id = "EXIT";
+
+        // Serialize message data to JSON
+        string json = JsonUtility.ToJson(message);
+        Debug.Log(json);
+        // Send the message
+        if (this.manager.Socket != null && this.manager.Socket.IsOpen)
+        {
+            this.manager.Socket.Emit(eventName, json);
+            Debug.Log("JSON data sent: " + json);
+        }
+        else
+        {
+            Debug.LogWarning("Socket is not connected.");
         }
     }
 
@@ -448,6 +470,12 @@ public class Message
     public UIData UIData { get; set; }
     public PlayerData PlayerData { get; set; }
     public List<string> BonusData { get; set; }
+}
+
+[Serializable]
+public class ExitData
+{
+    public string id;
 }
 
 [Serializable]
