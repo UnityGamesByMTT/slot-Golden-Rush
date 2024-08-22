3c3b93e5e1a4d738aeb03c3a5b61a026dc232189
EkanshBatra <>
2024-08-22 15:48:13 +0530

    special symbol animation fix

--- Assets/Scripts/Functionality/SlotBehaviour.cs
+++ Assets/Scripts/Functionality/SlotBehaviour.cs
@@ -655,7 +655,7 @@ public class SlotBehaviour : MonoBehaviour
     {
         List<int> y_points = null;
         List<int> points_anim = null;
-        if (LineId.Count > 0)
+        if (LineId.Count > 0 || points_AnimString.Count > 0) 
         {
             if (audioController) audioController.PlayWLAudio("win");
 
